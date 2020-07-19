05aeab38e03a504112a63c2231847b087e5f0b3a
chongyatming <chongyatming19930209@icloud.com>
2020-07-20 00:38:21 +0800

    v1 - init and mint

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 引入全部组件 Mint Ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+//Mint Ui End
+
 new Vue({
   router,
   store,
